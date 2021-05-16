55b9932d9bbd94fee2934668f4d8fcbe884fac1b
yushushu <yushushu@tal.com>
2021-05-16 22:32:14 +0800

    validator

--- app/validator/user.go
+++ app/validator/user.go
@@ -1,15 +1,5 @@
 package validator
 
-type User struct {
-	Id       int    `json:"id" form:"id"`
-	Mobile   int64  `json:"mobile"  form:"mobile" binding:"required" `
-	Email    string `json:"email"  form:"email"`
-	Nickname string `json:"nickname"  form:"nickname" binding:"required"`
-	Sex      int    `json:"sex"  form:"sex"`
-	Status   int    `json:"status"  form:"status"`
-	UserType int    `json:"user_type"  form:"user_type"`
-}
-
 type UserCreate struct {
 	Mobile   int64  `json:"mobile"  form:"mobile" binding:"required" `
 	Nickname string `json:"nickname"  form:"nickname" binding:"required"`
